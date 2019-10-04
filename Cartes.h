59664b73786425670bc12d31d18fc3d2d63bac8b
Arreme <a.reme@hotmail.com>
2019-10-04 12:17:10 +0200

    Update Cartes.h

--- Cartes.h
+++ Cartes.h
@@ -1,26 +1,31 @@
 #ifndef CARTA_H
 #define CARTA_H
-#include <string>
+#include <iostream>
+
 using namespace std;
 
 
 class Carta
 {
     public:
-        Carta();
-        virtual ~Carta();
-        string static donaNom(int numero);
-        string static donaPal(int pal);
-    protected:
-
-    private:
+        Carta(int, int);
+        int getNum();
+        string getPal();
+        string getNom();
         const static int COPES = 1;
         const static int ESPASES = 2;
         const static int BASTONS = 3;
         const static int OROS = 4;
+        void visualitza (bool);
+    protected:
+
+    private:
+
+        string static donaNom(int numero);
+        string static donaPal(int pal);
 
         const int NUMCARTA;
-        const int PAL;
+        const string PAL;
         const string NOMCARTA;
 };
 

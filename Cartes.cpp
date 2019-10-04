918ce13d52dacc37f8f459768d1034a02ef89e90
Arreme <a.reme@hotmail.com>
2019-10-04 10:56:38 +0200

    Update Cartes.cpp

--- Cartes.cpp
+++ Cartes.cpp
@@ -1,1 +1,27 @@
+#ifndef CARTA_H
+#define CARTA_H
+#include <string>
+using namespace std;
 
+
+class Carta
+{
+    public:
+        Carta();
+        virtual ~Carta();
+        string static donaNom(int numero);
+        string static donaPal(int pal);
+    protected:
+
+    private:
+        const static int COPES = 1;
+        const static int ESPASES = 2;
+        const static int BASTONS = 3;
+        const static int OROS = 4;
+
+        const int NUMCARTA;
+        const int PAL;
+        const string NOMCARTA;
+};
+
+#endif // CARTA_H

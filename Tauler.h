b48b9be496d6714650622d53c1b3ad51c8a3d822
Gerardmv <45603227+Gerardmv@users.noreply.github.com>
2019-10-09 12:22:56 +0200

    Update Tauler.h

--- Tauler.h
+++ Tauler.h
@@ -1,5 +1,7 @@
 #ifndef TAULER_H
 #define TAULER_H
+#include <Carta.h>
+#include <Baralla.h>
 
 
 class Tauler
@@ -7,10 +9,22 @@ class Tauler
     public:
         Tauler();
         virtual ~Tauler();
+        void visualitza(bool);
+        void extreureCarta();
+        void aparellar(int pos);
+        bool fiJocExit();
+        bool hiHaParelles();
+        Carta* getCarta(int posicio);
+        int getQuantesTaulell();
+        int getQuantesMunt();
 
     protected:
 
     private:
+    Carta*taulell[20];
+    int numCartesTaula;
+    Baralla*munt;
+
 };
 
 #endif // TAULER_H

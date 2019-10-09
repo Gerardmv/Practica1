4cd82ca02ed0f2c48d4f33ad30b88805a3b09986
Gerardmv <45603227+Gerardmv@users.noreply.github.com>
2019-10-09 10:23:30 +0200

    Update Baralla.h

--- Baralla.h
+++ Baralla.h
@@ -1,1 +1,24 @@
+#ifndef BARALLA_H
+#define BARALLA_H
+#include <Carta.h>
+class Baralla
+{
+    public:
+        Baralla();
+        virtual ~Baralla();
+        int getQuantes();
+        bool barallaBudia();
+        Carta*extreureCarta();
+        void visualitzar();
+        void destruir();
+
+    protected:
+
+    private:
+        static Carta*BARALLATAULA[40];
+        static int NUMdeCARTES;
+
+};
+
+#endif // BARALLA_H
 

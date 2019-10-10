2dcef5e853d673035c5e876d7ec467ea6fe1f5a6
Arreme <a.reme@hotmail.com>
2019-10-10 10:41:48 +0200

    Update Cartes.h

--- Cartes.h
+++ Cartes.h
@@ -9,23 +9,26 @@ class Carta
 {
     public:
         Carta(int, int);
+
         int getNum();
         string getPal();
         string getNom();
+        void visualitza (bool);
+
+        string static donaNom(int numero);
+        string static donaPal(int pal);
+
         const static int COPES = 1;
         const static int ESPASES = 2;
         const static int BASTONS = 3;
         const static int OROS = 4;
-        void visualitza (bool);
+
         bool operator==(Carta *);
         bool operator!=(Carta *);
 
     protected:
 
     private:
-
-        string static donaNom(int numero);
-        string static donaPal(int pal);
         int palAInt (string pal);
 
         const int NUMCARTA;
@@ -34,4 +37,3 @@ class Carta
 };
 
 #endif // CARTA_H
-

48e3403faf52764e2df5f235a89eafeff6630694
Arreme <a.reme@hotmail.com>
2019-10-10 11:38:50 +0200

    Update Baralla.cpp

--- Baralla.cpp
+++ Baralla.cpp
@@ -8,37 +8,17 @@ using namespace std;
 
 Baralla::Baralla()
 {
-
-    int pal=1,numcarta;
-    Baralla::NUMdeCARTES=1;
-    while(pal<=4){
-    numcarta=1;
-    while(numcarta<=12){
-    switch(pal) {
-    case Carta::COPES:
-    Baralla::BARALLATAULA[Baralla::NUMdeCARTES]= new Carta(numcarta,pal);
-             break;
-    case Carta::ESPASES :
-    Baralla:: BARALLATAULA[Baralla::NUMdeCARTES]= new Carta(numcarta,pal);
-             break;
-    case Carta::BASTONS :
-     Baralla::BARALLATAULA[Baralla::NUMdeCARTES]= new Carta(numcarta,pal);
-             break;
-    case Carta::OROS :
-     Baralla::BARALLATAULA[Baralla::NUMdeCARTES]= new Carta(numcarta,pal);
-             break;
-}
-numcarta++;
-Baralla::NUMdeCARTES++;
-}
-pal++;
-}
+    Baralla::NUMdeCARTES=0;
+    int pals[4]={Carta::BASTONS,Carta::COPES,Carta::ESPASES,Carta::OROS};
+    int numeros[10]={1,2,3,4,5,6,7,10,11,12};
+    for (int palAct:pals){
+        for (int numeroAct:numeros){
+            Baralla::BARALLATAULA[Baralla::NUMdeCARTES]= new Carta(numeroAct,palAct);
+            Baralla::NUMdeCARTES++;
+        }
+    }
 }
 
-Baralla::~Baralla()
-{
-    //dtor
-}
 int Baralla::getQuantes()
     {
         return Baralla::NUMdeCARTES;
@@ -51,37 +31,37 @@ if(getQuantes()==0) resultat=false;
 return resultat;
 }
 
-Carta*Baralla::extreureCarta()
+Carta* Baralla::extreureCarta()
 {
- int num;
- Carta*resultat;
+    int num;
+    Carta* resultat;
 
     srand(time(NULL));
     num=rand()%40;
 
-while(BARALLATAULA[num]==NULL){
-num=rand()%40;
-}
-resultat = BARALLATAULA[num];
-Baralla::BARALLATAULA[num]=NULL;
-Baralla::NUMdeCARTES=Baralla::NUMdeCARTES-1;
-return  resultat;
+    while(BARALLATAULA[num]==NULL){
+        num=rand()%40;
+    }
+    resultat = BARALLATAULA[num];
+    Baralla::BARALLATAULA[num]=NULL;
+    Baralla::NUMdeCARTES=Baralla::NUMdeCARTES-1;
+    return  resultat;
 }
 
 
 void Baralla::visualitzar()
 {
-Carta*aux = new Carta(1,1);
+Carta* aux = new Carta(1,1);
 for(int idx=0; idx>sizeof(Baralla::BARALLATAULA); idx++ ){
             aux = Baralla::BARALLATAULA[idx];
-            if(aux!=NULL){cout<<aux.getNom()<< "de" << aux.getPal()<<"";}//no funciona get()
+            if(aux!=NULL){cout<<aux->getNom()<< "de" << aux->getPal()<<"";}//no funciona get()
 }
 }
 void Baralla::destruir()
 {
-Carta aux;
-for(int idx==0; idx>sizeof(Baralla::BARALLATAULA); idx++ ){
+Carta* aux;
+for(int idx=0; idx>sizeof(Baralla::BARALLATAULA); idx++ ){
             aux = Baralla::BARALLATAULA[idx];
-            if(aux==null){aux.delete;}
+            if(aux==NULL){delete aux;}
         }
 }

3696b7ebaae3ed153484e067dcacd813acf70c27
Gerardmv <45603227+Gerardmv@users.noreply.github.com>
2019-10-11 09:57:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <Carta.h>
+#include <Tauler.h>
+#include <Baralla.h>
 
 using namespace std;
 
 int main()
 {
-    Carta *carta = new Carta();
-    cout << (*carta).donaNom(3) << endl;
-    cout << (*carta).donaPal(3) << endl;
-    return 0;
+    int accio=0;
+    int posicio=0;
+    Tauler* taulell = new Tauler;
+    Baralla->visualitzar();
+    //extreure tres cartes inicials
+    taulell->extreureCarta();
+    taulell->extreureCarta();
+    taulell->extreureCarta();
+
+    while(!taulell->fiJocExit()){
+        taulell->visualitza();
+        cout<<"1.Girar carta";
+        if(taulell->hiHaParelles()){
+            cout<<"2.Fer parella";
+        }
+        cout<<"Triar que vols fer";
+        accio=;
+        if(accio==1){taulell->extreureCarta();}
+        if(accio==2){
+            cout<<"indica posició"<<endl();
+            posicio=;
+            taulell->aparellar();
+        }
+    }
 }
